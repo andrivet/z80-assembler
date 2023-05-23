3be9ca51f5e21a4c980bbaa636d084fb81d866e5
Sebastien Andrivet <sebastien@andrivet.com>
2023-05-23 18:38:14 +0200

    Rename and reformat opcodes

--- apps/z80-assembler-app/src/app/opcodes.tsx
+++ apps/z80-assembler-app/src/app/opcodes.tsx
@@ -1,21 +1,21 @@
 import { Collapse, Table } from "react-daisyui";
 
 
-export function AppZ80Opcodes() {
+export function AppOpcodes() {
   return(
     <div className="flex flex-auto h-full relative mt-0">
       <div className="absolute top-0 bottom-0 left-0 right-0 overflow-auto">
-        <h1 className="font-bold mb-2 text-white">Z80 Instructions</h1>
+        <h1 className="font-bold mb-2 text-orange-400">Z80 Instructions</h1>
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">8-bit Load</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
+            <Table compact={true} zebra={true} width="100%">
               <Table.Head>
                 {['Opcode', 'Equivalent in C', 'Arguments']}
               </Table.Head>
               <Table.Body>
                 <Table.Row>{[<span>LD r1, r2</span>, <span>r1 = r2</span>, <span>r1, r2: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD r, n</span>, <span>r = n</span>, <span>r: A, B, C, D, E, H, L; n: 8-bit value</span>]}</Table.Row>
+                <Table.Row>{[<span>LD r, n</span>, <span>r = n</span>, <span>r: A, B, C, D, E, H, L<br />n: 8-bit value</span>]}</Table.Row>
                 <Table.Row>{[<span>LD r, (HL)</span>, <span>r = *HL</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
                 <Table.Row>{[<span>LD r, (IX+d)</span>, <span>r = IX[d]</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
                 <Table.Row>{[<span>LD r, (IY+d)</span>, <span>r = IY[d]</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
@@ -43,14 +43,14 @@ export function AppZ80Opcodes() {
         <Collapse checkbox={true} icon="arrow">
             <Collapse.Title className="font-bold">16-bit Load</Collapse.Title>
             <Collapse.Content>
-              <Table compact={false} zebra={true} width="100%">
+              <Table compact={true} zebra={true} width="100%">
                 <Table.Head>
                   {['Opcode', 'Equivalent in C', 'Arguments']}
                 </Table.Head>
                 <Table.Body>
-                  <Table.Row>{[<span>LD dd, nn</span>, <span>dd = nn</span>, <span>dd: BC, DE, HL, SP, IX, IY; nn: 16-bit value</span>]}</Table.Row>
-                  <Table.Row>{[<span>LD dd, (nn)</span>, <span>low(dd) = nn[0], high(dd) = nn[1]</span>, <span>dd: BC, DE, HL, SP, IX, IY; nn: 16-bit value</span>]}</Table.Row>
-                  <Table.Row>{[<span>LD (nn), dd</span>, <span>nn[0] = low(dd), nn[1] = high(dd)</span>, <span>dd: BC, DE, HL, SP, IX, IY; nn: 16-bit value</span>]}</Table.Row>
+                  <Table.Row>{[<span>LD dd, nn</span>, <span>dd = nn</span>, <span>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</span>]}</Table.Row>
+                  <Table.Row>{[<span>LD dd, (nn)</span>, <span>low(dd) = nn[0], high(dd) = nn[1]</span>, <span>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</span>]}</Table.Row>
+                  <Table.Row>{[<span>LD (nn), dd</span>, <span>nn[0] = low(dd), nn[1] = high(dd)</span>, <span>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</span>]}</Table.Row>
                   <Table.Row>{[<span>LD SP, HL</span>, <span>SP = HL</span>, <span></span>]}</Table.Row>
                   <Table.Row>{[<span>LD SP, IX</span>, <span>SP = IX</span>, <span></span>]}</Table.Row>
                   <Table.Row>{[<span>LD SP, IY</span>, <span>SP = IY</span>, <span></span>]}</Table.Row>
@@ -64,7 +64,7 @@ export function AppZ80Opcodes() {
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Exchange</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
+            <Table compact={true} zebra={true} width="100%">
               <Table.Head>
                 {['Opcode', 'Equivalent in C', 'Arguments']}
               </Table.Head>
@@ -81,7 +81,7 @@ export function AppZ80Opcodes() {
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Block Transfer and Search</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
+            <Table compact={true} zebra={true} width="100%">
               <Table.Head>
                 {['Opcode', 'Equivalent in C', 'Arguments']}
               </Table.Head>
@@ -211,7 +211,7 @@ export function AppZ80Opcodes() {
           </Collapse.Content>
         </Collapse>
 
-      <h1 className="font-bold mb-2 text-white">Pseudo-Instructions</h1>
+      <h1 className="font-bold mb-2 text-orange-400">Pseudo-Instructions</h1>
     </div>
     </div>
   );
