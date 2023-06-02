87c5c21eb90d8bbb41780fd85b842937b68093c2
Sebastien Andrivet <sebastien@andrivet.com>
2023-06-02 19:02:26 +0200

    Use simple tables

--- apps/z80-assembler-app/src/app/opcodes.tsx
+++ apps/z80-assembler-app/src/app/opcodes.tsx
@@ -17,205 +17,174 @@ export function AppOpcodes() {
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">8-bit Load</Collapse.Title>
           <Collapse.Content>
-            <Table compact={true} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span>LD r1, r2</span>,    <span>r1, r2: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD r, n</span>,      <span>r: A, B, C, D, E, H, L<br />n: 8-bit value</span>]}</Table.Row>
-                <Table.Row>{[<span>LD r, (HL)</span>,   <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD r, (IX+d)</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD r, (IY+d)</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD (HL), r</span>,   <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD (IX+d), r</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD (IY+d), r</span>, <span>r: A, B, C, D, E, H, L</span>]}</Table.Row>
-                <Table.Row>{[<span>LD (HL), n</span>,   <span>n: 8-bit value</span>]}</Table.Row>
-                <Table.Row>{[<span>LD (IX+d), n</span>, <span>n: 8-bit value</span>]}</Table.Row>
-                <Table.Row>{[<span>LD (IY+d), n</span>, <span>n: 8-bit value</span>]}</Table.Row>
-                <Table.Row>{[<span>LD A, (BC)</span>,   <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD A, (DE)</span>,   <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD A, (nn)</span>,   <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD (BC), A</span>,   <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD (DE), A</span>,   <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD (nn), A</span>,   <span>nn: 16-bit value</span>]}</Table.Row>
-                <Table.Row>{[<span>LD A, I</span>,      <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD A, R</span>,      <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD I, A</span>,      <span></span>]}</Table.Row>
-                <Table.Row>{[<span>LD R, A</span>,      <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+                <tr><td>LD r1, r2</td><td>r1, r2: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD r, n</td><td>r: A, B, C, D, E, H, L<br />n: 8-bit value</td></tr>
+                <tr><td>LD r, (HL)</td><td>r: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD r, (IX+d)</td><td>r: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD r, (IY+d)</td><td>r: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD (HL), r</td><td>r: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD (IX+d), r</td><td>r: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD (IY+d), r</td><td>r: A, B, C, D, E, H, L</td></tr>
+                <tr><td>LD (HL), n</td><td>n: 8-bit value</td></tr>
+                <tr><td>LD (IX+d), n</td><td>n: 8-bit value</td></tr>
+                <tr><td>LD (IY+d), n</td><td>n: 8-bit value</td></tr>
+                <tr><td>LD A, (BC)</td><td></td></tr>
+                <tr><td>LD A, (DE)</td><td></td></tr>
+                <tr><td>LD A, (nn)</td><td></td></tr>
+                <tr><td>LD (BC), A</td><td></td></tr>
+                <tr><td>LD (DE), A</td><td></td></tr>
+                <tr><td>LD (nn), A</td><td>nn: 16-bit value</td></tr>
+                <tr><td>LD A, I</td><td></td></tr>
+                <tr><td>LD A, R</td><td></td></tr>
+                <tr><td>LD I, A</td><td></td></tr>
+                <tr><td>LD R, A</td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
             <Collapse.Title className="font-bold">16-bit Load</Collapse.Title>
             <Collapse.Content>
-              <Table compact={true} zebra={true} width="100%">
-                <Table.Head>
-                  {['Opcode', 'Arguments']}
-                </Table.Head>
-                <Table.Body>
-                  <Table.Row>{[<span>LD dd, nn</span>,    <span>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</span>]}</Table.Row>
-                  <Table.Row>{[<span>LD dd, (nn)</span>,  <span>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</span>]}</Table.Row>
-                  <Table.Row>{[<span>LD (nn), dd</span>,  <span>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</span>]}</Table.Row>
-                  <Table.Row>{[<span>LD SP, HL</span>,    <span></span>]}</Table.Row>
-                  <Table.Row>{[<span>LD SP, IX</span>,    <span></span>]}</Table.Row>
-                  <Table.Row>{[<span>LD SP, IY</span>,    <span></span>]}</Table.Row>
-                  <Table.Row>{[<span>PUSH qq</span>,      <span>qq: BC, DE, HL, AF, IX, IY</span>]}</Table.Row>
-                  <Table.Row>{[<span>POP qq</span>,       <span>qq: BC, DE, HL, AF, IX, IY</span>]}</Table.Row>
-                </Table.Body>
-              </Table>
+              <table className="table table-zebra w-full">
+                <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+                <tbody>
+                  <tr><td>LD dd, nn</td><td>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</td></tr>
+                  <tr><td>LD dd, (nn)</td><td>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</td></tr>
+                  <tr><td>LD (nn), dd</td><td>dd: BC, DE, HL, SP, IX, IY<br />nn: 16-bit value</td></tr>
+                  <tr><td>LD SP, HL</td><td></td></tr>
+                  <tr><td>LD SP, IX</td><td></td></tr>
+                  <tr><td>LD SP, IY</td><td></td></tr>
+                  <tr><td>PUSH qq</td><td>qq: BC, DE, HL, AF, IX, IY</td></tr>
+                  <tr><td>POP qq</td><td>qq: BC, DE, HL, AF, IX, IY</td></tr>
+                </tbody>
+              </table>
             </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Exchange</Collapse.Title>
           <Collapse.Content>
-            <Table compact={true} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span>EX DE, HL</span>,    <span></span>]}</Table.Row>
-                <Table.Row>{[<span>EX AF, AF'</span>,   <span></span>]}</Table.Row>
-                <Table.Row>{[<span>EXX</span>,          <span></span>]}</Table.Row>
-                <Table.Row>{[<span>EX (SP), dd</span>,  <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+                <tr><td>EX DE, HL</td><td></td></tr>
+                <tr><td>EX AF, AF'</td><td></td></tr>
+                <tr><td>EXX</td><td></td></tr>
+                <tr><td>EX (SP), dd</td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Block Transfer and Search</Collapse.Title>
           <Collapse.Content>
-            <Table compact={true} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">8-bit Arithmetic</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">16-bit Arithmetic</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Rotate and Shift</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Bit Manipulation</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Jump, Call and Return</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Input and Output</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">CPU Control</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
         <Collapse checkbox={true} icon="arrow">
           <Collapse.Title className="font-bold">Pseudo Instructions</Collapse.Title>
           <Collapse.Content>
-            <Table compact={false} zebra={true} width="100%">
-              <Table.Head>
-                {['Opcode', 'Arguments']}
-              </Table.Head>
-              <Table.Body>
-                <Table.Row>{[<span></span>, <span></span>]}</Table.Row>
-              </Table.Body>
-            </Table>
+            <table className="table table-zebra w-full">
+              <thead><tr className="bg-neutral-900"><th>Opcode</th><th>Arguments</th></tr></thead>
+              <tbody>
+              <tr><td></td><td></td></tr>
+              </tbody>
+            </table>
           </Collapse.Content>
         </Collapse>
 
