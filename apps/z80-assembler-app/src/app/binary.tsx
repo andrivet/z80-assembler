e454387060667be64e2da43438a62b0d91218d54
Sebastien Andrivet <sebastien@andrivet.com>
2024-11-20 13:22:26 +0100

    Fix app (Table)

--- apps/z80-assembler-app/src/app/binary.tsx
+++ apps/z80-assembler-app/src/app/binary.tsx
@@ -21,7 +21,7 @@ export default function AppBinary({chunks}: AppBinaryProps) {
       <h1 className="font-bold mb-2 text-orange-400">Machine Code</h1>
       <div className="flex flex-auto h-full relative mt-0">
         <div className="absolute top-0 bottom-0 left-0 right-0 overflow-auto">
-          <Table compact={true} zebra={true}>
+          <Table zebra={true}>
             <Table.Head>{['Address', 'Bytes']}</Table.Head>
             <Table.Body>
               {chunks?.map((data, index) => (
