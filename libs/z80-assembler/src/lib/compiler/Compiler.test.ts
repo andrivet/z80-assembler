andrivet/z80-assembler

commit 4bcb12ea82d38f83ce2079f8635909d0c5b5c57a

Adjust grammar and tests to follow the doc

libs/z80-assembler/src/lib/compiler/Compiler.test.ts
@@ -74,15 +74,56 @@ test("Loading hexadecimal with 0x", () => {
   expect(bytes).toEqual([0x3e, 0x55]);
 });
 
+test("Loading hexadecimal with 0X", () => {
+  const bytes = compileCode(' ld a, 0X55');
+  expect(bytes).toEqual([0x3e, 0x55]);
+});
+
 test("Loading hexadecimal with h", () => {
   const bytes = compileCode(' ld a, 55h');
   expect(bytes).toEqual([0x3e, 0x55]);
+});
+
+test("Loading hexadecimal with H", () => {
+  const bytes = compileCode(' ld a, 55H');
+  expect(bytes).toEqual([0x3e, 0x55]);
+});
+
+test("Loading hexadecimal with only letters", () => {
+  const bytes = compileCode(' ld a, abh');
+  expect(bytes).toEqual([0x3e, 0xab]);
+});
+
+test("Loading hexadecimal with letters prefixed with 0", () => {
+  const bytes = compileCode(' ld a, 0abh');
+  expect(bytes).toEqual([0x3e, 0xab]);
+});
+
+test("Loading hexadecimal with mix of cases", () => {
+  const bytes = compileCode(' ld a, Abh');
+  expect(bytes).toEqual([0x3e, 0xab]);
 });
 
 test("Loading binary with 0b", () => {
   const bytes = compileCode(' ld a, 0b10101010');
   expect(bytes).toEqual([0x3e, 0xAA]);
 });
+
+test("Loading binary with 0B", () => {
+  const bytes = compileCode(' ld a, 0B10101010');
+  expect(bytes).toEqual([0x3e, 0xAA]);
+});
+
+test("Loading binary with b suffix", () => {
+  const bytes = compileCode(' ld a, 10101010b');
+  expect(bytes).toEqual([0x3e, 0xAA]);
+});
+
+test("Loading binary with B suffix", () => {
+  const bytes = compileCode(' ld a, 10101010B');
+  expect(bytes).toEqual([0x3e, 0xAA]);
+});
+
 
 test("Loading binary with %", () => {
   const bytes = compileCode(' ld a, %10101010');
@@ -99,13 +140,28 @@ test("Loading octal with 0o", () => {
   expect(bytes).toEqual([0x3e, 0x55]);
 });
 
+test("Loading octal with 0O", () => {
+  const bytes = compileCode(' ld a, 0O125');
+  expect(bytes).toEqual([0x3e, 0x55]);
+});
+
 test("Loading octal with q", () => {
   const bytes = compileCode(' ld a, 125q');
   expect(bytes).toEqual([0x3e, 0x55]);
 });
 
+test("Loading octal with Q", () => {
+  const bytes = compileCode(' ld a, 125Q');
+  expect(bytes).toEqual([0x3e, 0x55]);
+});
+
 test("Loading octal with o", () => {
   const bytes = compileCode(' ld a, 125o');
+  expect(bytes).toEqual([0x3e, 0x55]);
+});
+
+test("Loading octal with O", () => {
+  const bytes = compileCode(' ld a, 125O');
   expect(bytes).toEqual([0x3e, 0x55]);
 });
 
@@ -1684,15 +1740,6 @@ test("Instruction OTDR", () => {
   expect(bytes).toEqual([0xED, 0xBB]);
 });
 
-test("Statement EQU", () => {
-  const bytes = compileCode('label1 EQU $55\nDB label1');
-  expect(bytes).toEqual([0x55]);
-});
-
-test("Statement EQU with colon", () => {
-  const bytes = compileCode('label1: EQU $55\nDB label1');
-  expect(bytes).toEqual([0x55]);
-});
 
 // -------------------------------------------------------
 // Labels
@@ -1771,6 +1818,11 @@ test("Declaring string", () => {
   expect(bytes).toEqual([0x2D, 0x2A, 0x31, 0x31, 0x34, 0x00, 0x3C, 0x34, 0x37, 0x31, 0x29]);
 });
 
+test("Declaring string with simple quotes", () => {
+  const bytes = compileCode('db \'HELLO WORLD\'');
+  expect(bytes).toEqual([0x2D, 0x2A, 0x31, 0x31, 0x34, 0x00, 0x3C, 0x34, 0x37, 0x31, 0x29]);
+});
+
 test("Declaring strings", () => {
   const bytes = compileCode('db "HELLO", "WORLD"');
   expect(bytes).toEqual([0x2D, 0x2A, 0x31, 0x31, 0x34, 0x3C, 0x34, 0x37, 0x31, 0x29]);
@@ -1824,9 +1876,39 @@ test("Declaring block with length dependent of the size", () => {
 // Directives
 // -------------------------------------------------------
 
+test("Directive EQU", () => {
+  const bytes = compileCode('label1 EQU $55\nDB label1');
+  expect(bytes).toEqual([0x55]);
+});
+
+test("Directive EQU with colon", () => {
+  const bytes = compileCode('label1: EQU $55\nDB label1');
+  expect(bytes).toEqual([0x55]);
+});
+
+test("Directive EQU with dot", () => {
+  const bytes = compileCode('label1 .EQU $55\nDB label1');
+  expect(bytes).toEqual([0x55]);
+});
+
+test("Directive EQU with period and colon", () => {
+  const bytes = compileCode('label1: .EQU $55\nDB label1');
+  expect(bytes).toEqual([0x55]);
+});
+
 test("Origin directive", () => {
   const bytes = compileCode('org $5544\nlabel1:\nld hl, label1');
   expect(bytes).toEqual([0x21, 0x44, 0x55]);
+});
+
+test("Directive EQU with equal symbol", () => {
+  const bytes = compileCode('label1 = $55\nDB label1');
+  expect(bytes).toEqual([0x55]);
+});
+
+test("Directive EQU with colon and equal symbol", () => {
+  const bytes = compileCode('label1: = $55\nDB label1');
+  expect(bytes).toEqual([0x55]);
 });
 
 test("Origin directive with dot", () => {
@@ -1915,6 +1997,11 @@ test("Device", () => {
 test("Device with dot", () => {
   const bytes = compileCode('.device test');
   expect(bytes).toEqual([]);
+});
+
+test("End Directive", () => {
+  const bytes = compileCode('ld a,1\nend\nld a,2');
+  expect(bytes).toEqual([0x3E, 0x01]);
 });
 
 

libs/z80-assembler/src/lib/compiler/Generator.ts
@@ -161,6 +161,7 @@ function generateLines(address: number, infos: LinesInfo[]): GenerationData {
   // On débute avec un tableau vide d'octets.
   let bytes: bytes = [];
   let sld = '';
+  let end = false;
 
   // For each file...
   // Pour chaque fichier...
@@ -201,9 +202,18 @@ function generateLines(address: number, infos: LinesInfo[]): GenerationData {
         }
         break;
       }
+
+      // Special case for end directive
+      if(line.statement.kind === ASTKinds.Statement_3 && line.statement.dir.kind === ASTKinds.Directive_5) {
+        end = true;
+        break;
+      }
+
+      if(end) break;
       lineNumber += 1; // Next line
     }
   }
+
   return {bytes: bytes, sld: sld, address: address};
 }
 

libs/z80-assembler/src/lib/grammar/Parse.ts
@@ -78,7 +78,8 @@ export function parseNumber(pos: PosInfo, str: string, base: number, nbBytes: nu
  */
 export function parseSimpleEscape(pos: PosInfo, c: string): number[] {
   switch(c) {
-    case '"': return [0x0B];
+    case 'n':  return [0x0B];
+    case '"':  return [0x0B];
     default:  throw new CompilationError({filename: parseData.fileName, pos: pos},
       `Invalid escape: \\${c}`);
   }

commit 7111c073b7faadcb89a2db0d09bd7776a23eccc5

Fix bugs introduced in commit 4bcb12ea82d38f83ce2079f8635909d0c5b5c57a

apps/z80-assembler-app/src/app/editor.tsx
@@ -179,7 +179,9 @@ function AppEditor(props: AppEditorProps, ref: React.ForwardedRef<AppEditorHandl
   }
 
   const asmLinter = linter(view => {
-    return props.errors == null ? [] : props.errors?.map(value => {
+    return props.errors == null ? [] : props.errors
+      ?.filter(value => codeFiles[currentFile].filepath === value.position.filename)
+      .map(value => {
       const from = view.state.doc.line(value.position.pos.line).from + value.position.pos.offset;
       return {
         from: from,

libs/z80-assembler/src/lib/compiler/Compiler.ts
@@ -25,7 +25,7 @@ import {
 /**
  * Type of the internal data.
  */
-interface ParseDate {
+interface ParseData {
   // The name of the output file as declared by the output directive.
   outputName: string,
   // The name of the SLD file as declared by the output directive.
@@ -45,7 +45,7 @@ interface ParseDate {
  * Unfortunately, tsPEG does not allow to declare a context for the parsing.
  * So instead, we use this ugly global.
  */
-const parseData: ParseDate = {
+const parseData: ParseData = {
   outputName: "",
   sldName: "",
   deviceName: "",

libs/z80-assembler/src/lib/grammar/z80.peg
@@ -107,9 +107,10 @@ octal :=
     raw='[0-7]+' {'q' | 'o'}
 
 // An hexadecimal value with the different allowed syntaxes
+// To avoid ambiguity, hexadecimal values are not allowed to be followed by an alphanumerical character
 hexadecimal :=
     {'0x' | '\$' | '#'} raw='[0-9a-f]+' |
-    raw='[0-9a-f]+' 'h'
+    raw='[0-9a-f]+' 'h' !'[0-9a-z]'
 
 // Different names used in the Z80 CPU User Manual for 8-bit values
 d := n
@@ -185,8 +186,8 @@ Directive :=
 
 // Names not allowed for a label
 ForbiddenNames :=
-    '.\?block|.\?equ|\.?org|\.?include|\.?output|\.?device|\.?end|.\?byte' |
-    'call|cpdr|cpir|defb|defm|defs|defw|djnz|eq|halt|indr|inir|lddr|ldir|otdr|otir|outd|outi|push|reti|retn|rlca|rrca|.\?word' |
+    '\.?block|\.?equ|\.?org|\.?include|\.?output|\.?device|\.?end|\.?byte' |
+    'call|cpdr|cpir|defb|defm|defs|defw|djnz|eq|halt|indr|inir|lddr|ldir|otdr|otir|outd|outi|push|reti|retn|rlca|rrca|\.?word' |
     'adc|add|and|bit|ccf|cpd|cpi|cpl|daa|dec|exx|inc|ind|ini|ldd|ldi|neg|nop|org|out|pop|res|ret|rla' |
     'rlc|rld|rra|rrc|rrd|rst|sbc|scf|set|sla|sra|srl|sub|xor' |
     'cp|db|di|dm|ds|dw|ei|eq|ex|im|in|jp|jr|ld|or|rl|rr'
@@ -195,7 +196,7 @@ ForbiddenNames :=
 ForbiddenLabel :=
     ForbiddenNames '[ \t:\r\n]'
 
-// A label can't have a forbidden name. It is a name of the special pseudo label $ (PC).
+// A label can't have a forbidden name. It is a name or the special pseudo label $ (PC).
 Label :=
     !ForbiddenLabel pos=@ name='[a-z_\.][a-z0-9_!\?#@\.\$]*|\$'
 
@@ -1141,15 +1142,15 @@ OutputInstruction :=
 
 // Byte directive
 ByteDirective :=
-    'db' | 'dm' | '.\?byte' | 'defb' | 'defm'
+    'db' | 'dm' | '\.?byte' | 'defb' | 'defm'
 
 // Word directive
 WordDirective :=
-    'dw' | '.\?word' | 'defw'
+    'dw' | '\.?word' | 'defw'
 
 // Block directive
 BlockDirective :=
-    '.\?block' | 'defs' | 'ds'
+    '\.?block' | 'defs' | 'ds'
 
 // A Byte directive contains either characters, strings, or expressions
 ByteValue :=

libs/z80-assembler/src/lib/grammar/z80.ts
@@ -4169,6 +4169,7 @@ export class Parser {
                 if (true
                     && ($scope$raw = this.regexAccept(String.raw`(?:[0-9a-f]+)`, $$dpth + 1, $$cr)) !== null
                     && this.regexAccept(String.raw`(?:h)`, $$dpth + 1, $$cr) !== null
+                    && this.negate(() => this.regexAccept(String.raw`(?:[0-9a-z])`, $$dpth + 1, $$cr)) !== null
                 ) {
                     $$res = {kind: ASTKinds.hexadecimal_2, raw: $scope$raw};
                 }
@@ -4485,10 +4486,10 @@ export class Parser {
         ]);
     }
     public matchForbiddenNames_1($$dpth: number, $$cr?: ErrorTracker): Nullable<ForbiddenNames_1> {
-        return this.regexAccept(String.raw`(?:.\?block|.\?equ|\.?org|\.?include|\.?output|\.?device|\.?end|.\?byte)`, $$dpth + 1, $$cr);
+        return this.regexAccept(String.raw`(?:\.?block|\.?equ|\.?org|\.?include|\.?output|\.?device|\.?end|\.?byte)`, $$dpth + 1, $$cr);
     }
     public matchForbiddenNames_2($$dpth: number, $$cr?: ErrorTracker): Nullable<ForbiddenNames_2> {
-        return this.regexAccept(String.raw`(?:call|cpdr|cpir|defb|defm|defs|defw|djnz|eq|halt|indr|inir|lddr|ldir|otdr|otir|outd|outi|push|reti|retn|rlca|rrca|.\?word)`, $$dpth + 1, $$cr);
+        return this.regexAccept(String.raw`(?:call|cpdr|cpir|defb|defm|defs|defw|djnz|eq|halt|indr|inir|lddr|ldir|otdr|otir|outd|outi|push|reti|retn|rlca|rrca|\.?word)`, $$dpth + 1, $$cr);
     }
     public matchForbiddenNames_3($$dpth: number, $$cr?: ErrorTracker): Nullable<ForbiddenNames_3> {
         return this.regexAccept(String.raw`(?:adc|add|and|bit|ccf|cpd|cpi|cpl|daa|dec|exx|inc|ind|ini|ldd|ldi|neg|nop|org|out|pop|res|ret|rla)`, $$dpth + 1, $$cr);
@@ -9937,7 +9938,7 @@ export class Parser {
         return this.regexAccept(String.raw`(?:dm)`, $$dpth + 1, $$cr);
     }
     public matchByteDirective_3($$dpth: number, $$cr?: ErrorTracker): Nullable<ByteDirective_3> {
-        return this.regexAccept(String.raw`(?:.\?byte)`, $$dpth + 1, $$cr);
+        return this.regexAccept(String.raw`(?:\.?byte)`, $$dpth + 1, $$cr);
     }
     public matchByteDirective_4($$dpth: number, $$cr?: ErrorTracker): Nullable<ByteDirective_4> {
         return this.regexAccept(String.raw`(?:defb)`, $$dpth + 1, $$cr);
@@ -9956,7 +9957,7 @@ export class Parser {
         return this.regexAccept(String.raw`(?:dw)`, $$dpth + 1, $$cr);
     }
     public matchWordDirective_2($$dpth: number, $$cr?: ErrorTracker): Nullable<WordDirective_2> {
-        return this.regexAccept(String.raw`(?:.\?word)`, $$dpth + 1, $$cr);
+        return this.regexAccept(String.raw`(?:\.?word)`, $$dpth + 1, $$cr);
     }
     public matchWordDirective_3($$dpth: number, $$cr?: ErrorTracker): Nullable<WordDirective_3> {
         return this.regexAccept(String.raw`(?:defw)`, $$dpth + 1, $$cr);
@@ -9969,7 +9970,7 @@ export class Parser {
         ]);
     }
     public matchBlockDirective_1($$dpth: number, $$cr?: ErrorTracker): Nullable<BlockDirective_1> {
-        return this.regexAccept(String.raw`(?:.\?block)`, $$dpth + 1, $$cr);
+        return this.regexAccept(String.raw`(?:\.?block)`, $$dpth + 1, $$cr);
     }
     public matchBlockDirective_2($$dpth: number, $$cr?: ErrorTracker): Nullable<BlockDirective_2> {
         return this.regexAccept(String.raw`(?:defs)`, $$dpth + 1, $$cr);

libs/z80-assembler/src/lib/compiler/Compiler.test.ts
@@ -161,7 +161,6 @@ test("Loading octal with O", () => {
   expect(bytes).toEqual([0x3e, 0x55]);
 });
 
-
 test("Loading label with colon", () => {
   const bytes = compileCode('ld a, 1\nlabel1:\nld a, label1');
   expect(bytes).toEqual([0x3e, 0x01, 0x3e, 0x02]);
@@ -170,6 +169,12 @@ test("Loading label with colon", () => {
 test("Loading label without colon", () => {
   const bytes = compileCode('ld a, 1\nlabel1\nld a, label1');
   expect(bytes).toEqual([0x3e, 0x01, 0x3e, 0x02]);
+});
+
+// "chances" starts wich "ch", that cam be confused with C in hexadecimal
+test("Loading ambiguous label", () => {
+  const bytes = compileCode('chances eq 0x44\nld a, chances');
+  expect(bytes).toEqual([0x3e, 0x44]);
 });
 
 // -------------------------------------------------------
@@ -393,6 +398,16 @@ test("Instruction LD A, (DE)", () => {
 test("Instruction LD A, (nn)", () => {
   const bytes = compileCode('LD A, (0x1234)');
   expect(bytes).toEqual([0x3A, 0x34, 0x12]);
+});
+
+test("Instruction LD A, (label)", () => {
+  const bytes = compileCode('CHANCES EQ 0x44\nLD A, (CHANCES)');
+  expect(bytes).toEqual([0x3A, 0x44, 0x00]);
+});
+
+test("Instruction LD A, (hex) with ambiguity", () => {
+  const bytes = compileCode('CH EQ 0x44\nLD A, (CH)');
+  expect(bytes).toEqual([0x3A, 0x0C, 0x00]);
 });
 
 test("Instruction LD (BC), A", () => {
@@ -1795,6 +1810,15 @@ test("Label redefined", () => {
   compileWithError('lbl: db $00\nlbl: db $01\ndb lbl');
 });
 
+test("Label forbidden", () => {
+  compileWithError('equ: db $00\nld a, 0x44');
+});
+
+test("Label almost forbidden", () => {
+  const bytes = compileCode('_equ: db $00\nld a, 0x44');
+  expect(bytes).toEqual([0x00, 0x3E, 0x44]);
+});
+
 // -------------------------------------------------------
 // Data
 // -------------------------------------------------------
@@ -2115,8 +2139,3 @@ test.asm|4||0|-1|10|L|,size,,+equ
 test.asm|5||0|0|10|T|
 `);
 });
-
-// -------------------------------------------------------
-// Full code
-// -------------------------------------------------------
-